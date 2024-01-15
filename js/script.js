0531e83111c604483bc97a770a2ea3796124e125
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-15 14:51:55 -0300

    progresso + correcoes

---
+++ js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Texto a ser digitado
+    var textToType = "Desenvolvedor Front-end";
+  
+    // Elemento onde o efeito de digitação será aplicado
+    var typingElement = document.querySelector(".typing-effect");
+  
+    // Função para adicionar o efeito de digitação
+    function typeEffect(text, index) {
+      typingElement.textContent = text.slice(0, index + 1);
+  
+      // Adiciona uma classe após digitar o último caractere
+      if (index === text.length - 1) {
+        typingElement.classList.add("last-letters");
+      }
+    }
+  
+    // Função para simular o efeito de digitação ao longo do tempo
+    function startTypingEffect() {
+      for (let i = 0; i < textToType.length; i++) {
+        setTimeout(function() {
+          typeEffect(textToType, i);
+        }, i * 120); // Ajuste o intervalo de tempo conforme necessário
+      }
+    }
+
+    startTypingEffect();
+  });
+
+  // DOWNLOAD CV BTN
+
+  const btnCV = document.querySelector("#btn-cv");
+
+  btnCV.addEventListener("click", () => {
+    window.open("assets/CV-THIAGOTAVARES.pdf", "_blank");
+  })
